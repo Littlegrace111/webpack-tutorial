20254a35490896808228bf42bcddee0dd3a0d18f
littlegrace <dongni.wx@alibaba-inc.com>
2018-02-28 16:38:34 +0800

    Using html-webpack-plugin and fix its bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,21 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './src/index.js',
         print: './src/print.js',
     },
+    devtool: 'inline-source-map',
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({title: 'Development'}),
+    ],
     module: {
         rules: [
             {
