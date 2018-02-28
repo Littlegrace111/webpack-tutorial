51a251377bcd75e3644546f281f4a673df53eb34
littlegrace <dongni.wx@alibaba-inc.com>
2018-02-28 15:58:42 +0800

    Output multiple bundles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -12,8 +14,11 @@ function component() {
     // Add the image to our existing div
     var myIcon = new Image();
     myIcon.src = Icon;
-
     element.appendChild(myIcon);
+
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
---
+++ src/print.js
@@ -0,0 +1,4 @@
+export default function printMe() {
+    "use strict";
+    console.log('I get called from print.js!');
+}
